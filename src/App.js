b7a5906a63f54a899030a8f45c2b9b6cf51588a3
max18 <max18071998@gmail.com>
2025-03-14 20:28:50 +0300

    Redux Hooks  Избавляемся от connect()  Подключение useSelector, useDispatch  useSelector перекидываем в Home.jsx  React.useCallback  React.memo()  JSON Server

--- src/App.js
+++ src/App.js
@@ -3,39 +3,24 @@ import {Header} from "./Components";
 import {Cart, Home} from "./Components/Pages";
 import {Route, Routes} from "react-router-dom";
 import axios from "axios";
-import {connect} from "react-redux";
-import {setPizzas as setPizzasAction} from "./redux/actions/pizzas";
+import {useDispatch} from "react-redux";
+import {setPizzas} from "./redux/actions/pizzas";
 
 
-class App extends React.Component {
-    componentDidMount() {
+function App() {
+    const dispatch = useDispatch();
+    React.useEffect(() => {
         axios.get('http://localhost:3000/db.json').then(({data}) => {
-            this.props.setPizzas(data.pizzas);
-        });
-    }
-    render() {
-        return (<div className="wrapper">
-            <Header/>
-            <div className="content">
-                <Routes>
-                    <Route path="/" element={<Home items={this.props.items}/>}/>
-                    <Route path="/cart" element={<Cart/>}/>
-                </Routes>
-            </div>
-        </div>)
-    }
+            dispatch(setPizzas(data.pizzas));
+    });})
+    return (<div className="wrapper">
+        <Header/>
+        <div className="content">
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/cart" element={<Cart/>}/>
+            </Routes>
+        </div>
+    </div>)
 }
-
-const mapStateToProps = (state) => {
-    return {
-        items: state.pizzas.items,
-        filter: state.pizzas.filter,
-    }
-}
-const mapDispatchToProps = (dispatch) => {
-    return {
-        setPizzas: (items) => dispatch(setPizzasAction(items)),
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,31 +1,46 @@
 import {Categories, SortPopup,PizzaBlock} from "../index";
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {setCategory} from "../../redux/actions/filters";
 
-function Home({items}) {
+const categoryNames = [
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+]
+const sortItems=[
+    {name: 'популярности', type: 'popular'},
+    {name: 'цене', type: 'price'},
+    {name: 'алфавиту', type: 'alphabet'}
+
+]
+
+function Home() {
+    const dispatch = useDispatch();
+    const items = useSelector(({pizzas}) => pizzas.items);
+    const onSelectCategory = React.useCallback((index) => {
+        dispatch(setCategory(index));
+    },[]);
+
     return (
         <div className="container">
             <div className="content__top">
-                <Categories onClick={(name) => console.log(name)} items={[
-                    'Мясные',
-                    'Вегетарианская',
-                    'Гриль',
-                    'Острые',
-                    'Закрытые',
-                ]}/>
-                <SortPopup items={[
-                    {name:'популярности', type: 'popular'},
-                    {name:'цене', type: 'price'},
-                    {name:'алфавиту', type: 'alphabet'}
-                ]}/>
+                <Categories
+                    onClickItem={onSelectCategory}
+                    items={categoryNames}/>
+                <SortPopup items={sortItems}/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
                 {
-                    items.map((obj)=>(
-                        <PizzaBlock  key={obj.id} {...obj}/>))
+                    items && items.map((obj) => (
+                        <PizzaBlock key={obj.id} {...obj}/>))
                 }
             </div>
         </div>
     )
 }
+
 export default Home
